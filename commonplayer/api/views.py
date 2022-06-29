d02b7ff5287f1c436bb9ec9e4acd00a0059e18c4
SMassalski <stanislaw.massalski@gmail.com>
2022-06-29 14:43:34 +0200

    MAINT: Added BrowserClientView inheritance
    LifecycleView, ControlView and NavigateView
    inherit from BrowserClientView to avoid
    repeating code. Client is instantiated in
    the constructor. BrowserClientView
    implements sending and receiving data via
    the client

--- commonplayer/api/views.py
+++ commonplayer/api/views.py
@@ -6,52 +6,50 @@ from .client import BrowserClient
 from .serializers import CommandSerializer, NavigateSerializer, \
     ControlSerializer
 
-# TODO: Package repeating code
-
 
 class BrowserClientView(APIView):
     
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.client = BrowserClient()
-
-
-class NavigateView(BrowserClientView):
-    """Navigate to or get current url
-    https://www.youtube.com/watch?v=AvtVuHMqOOM"""
-    # Added url for easy access when manually testing
-    serializer_class = NavigateSerializer
-
-    # noinspection PyMethodMayBeStatic
-    def get(self, _):
-        """Get the current url of the browser"""
-        data = {
-            'command': BrowserClient.GET
-        }
-        with self.client:
-            browser_response = self.client.send(data)
-        return Response(browser_response)
-
-    # noinspection PyMethodMayBeStatic
-    def post(self, request):
-        """Go to a url"""
-        data = {
-            'command': BrowserClient.GOTO,
-            'value': request.data.get('url'),
-        }
+        
+    # TODO: Handling server failures ({'ok': False} responses)
+    def send_to_browser_server(self, data):
         with self.client:
             browser_response = self.client.send(data)
         if not browser_response.get('ok'):
             return Response(browser_response,
                             status.HTTP_500_INTERNAL_SERVER_ERROR)
         return Response(browser_response)
+        
+
+class NavigateView(BrowserClientView):
+    """Navigate to or get current url
+    https://www.youtube.com/watch?v=AvtVuHMqOOM"""
+    # Added url for easy access when manually testing
+    
+    serializer_class = NavigateSerializer
+
+    def get(self, _):
+        """Get the current url of the browser"""
+        data = {
+            'command': BrowserClient.GET
+        }
+        return self.send_to_browser_server(data)
+
+    def post(self, request):
+        """Go to a url"""
+        data = {
+            'command': BrowserClient.GOTO,
+            'value': request.data.get('url'),
+        }
+        return self.send_to_browser_server(data)
     
     
 class LifecycleView(BrowserClientView):
     """Control the browser's lifecycle"""
     serializer_class = CommandSerializer
 
-    # noinspection PyMethodMayBeStatic
     def post(self, request):
         """Send a command to the server
         
@@ -60,12 +58,7 @@ class LifecycleView(BrowserClientView):
         - End: close a browser
         """
         
-        with self.client:
-            browser_response = self.client.send(request.data)
-        if not browser_response.get('ok'):
-            return Response(browser_response,
-                            status.HTTP_500_INTERNAL_SERVER_ERROR)
-        return Response(browser_response)
+        return self.send_to_browser_server(request.data)
     
         
 class ControlView(BrowserClientView):
@@ -73,13 +66,7 @@ class ControlView(BrowserClientView):
     
     serializer_class = ControlSerializer
     
-    # noinspection PyMethodMayBeStatic
     def post(self, request):
         data = dict(command=BrowserClient.CONTROL)
         data['value'] = request.data.get('action')
-        with self.client:
-            browser_response = self.client.send(data)
-        if not browser_response.get('ok'):
-            return Response(browser_response,
-                            status.HTTP_500_INTERNAL_SERVER_ERROR)
-        return Response(browser_response)
+        return self.send_to_browser_server(data)
